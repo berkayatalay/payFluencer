c06bb8bb79cb132edc7608c5e715ce79a9fc1153
Berkay Atalay <atalay.berkay.all@gmail.com>
2024-09-16 21:23:08 +0300

    First Commit

---
+++ src/payFluencer/Domain/Entities/Influencer.cs
@@ -0,0 +1,12 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Influencer : IEntity<Guid>
+{
+    public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
